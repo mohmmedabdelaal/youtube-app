75e0bec8c0e66d9072ccd010f869fd0d2f15560f
Moelaal <medo.elaal@gmail.com>
2022-07-13 18:17:49 +0200

    reafactor searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 
-const SearchBar = (props) => {
+const SearchBar = ({ onFormSubmit }) => {
   const [term, setTerm] = useState('');
-  //  state = { term: '' };
 
-  const onInputChange = (event) => {
-    setTerm(event.target.value);
-  };
-
-  const onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
-    props.onFormSubmit(term);
+    onFormSubmit(term);
   };
 
   return (
     <div className="search-bar ui segment">
-      <form onSubmit={onFormSubmit} className="ui form">
+      <form onSubmit={onSubmit} className="ui form">
         <div className="field">
           <label>Video Search</label>
-          <input type="text" value={term} onChange={onInputChange} />
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
         </div>
       </form>
     </div>
